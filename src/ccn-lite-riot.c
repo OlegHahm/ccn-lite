f64994de1a354fc1b6539d93575a268df1687f73
Oleg Hahm <oleg@hobbykeller.org>
2015-11-09 14:36:26 +0100

    SQUASH ME: configure interface to use CCN nettype

--- src/ccn-lite-riot.c
+++ src/ccn-lite-riot.c
@@ -159,11 +159,15 @@ ccnl_ll_TX(struct ccnl_relay_s *ccnl, struct ccnl_if_s *ifc,
 int
 ccnl_open_netif(kernel_pid_t if_pid, gnrc_nettype_t netreg_type)
 {
+    /* configure the interface to use the specified nettype protocol */
+    gnrc_netapi_set(if_pid, NETOPT_PROTO, 0, &netreg_type, sizeof(gnrc_nettype_t));
+    /* register for this nettype */
     gnrc_netreg_entry_t ne;
     ne.demux_ctx =  GNRC_NETREG_DEMUX_CTX_ALL;
     ne.pid = if_pid;
     return gnrc_netreg_register(netreg_type, &ne);
 }
+
 int
 ccnl_open_udpdev(int port)
 {
