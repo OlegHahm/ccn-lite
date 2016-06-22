baca4f410a65e8760b2b571fc8bb7777b633de7e
Oleg Hahm <oleg@hobbykeller.org>
2016-06-22 10:37:19 +0200

    DIRTY: count faces

--- src/ccnl-core.c
+++ src/ccnl-core.c
@@ -358,6 +358,8 @@ ccnl_face_enqueue(struct ccnl_relay_s *ccnl, struct ccnl_face_s *to,
     struct ccnl_buf_s *msg;
     if (buf == NULL) {
         DEBUGMSG_CORE(ERROR, "enqueue face: buf most not be NULL\n");
+        extern void ccnl_count_faces(struct ccnl_relay_s *ccnl);
+        ccnl_count_faces(ccnl);
         extern void ccnl_pit_size(struct ccnl_relay_s *ccnl);
         ccnl_pit_size(ccnl);
         return -1;
@@ -1134,6 +1136,16 @@ void ccnl_pit_size(struct ccnl_relay_s *ccnl)
         size++;
     }
     printf("PIT size: %u\n", size);
+}
+
+void ccnl_count_faces(struct ccnl_relay_s *ccnl)
+{
+    unsigned size = 0;
+    struct ccnl_face_s *f;
+    for (f = ccnl->faces; f; f = f->next) {
+        size++;
+    }
+    printf("No. of faces: %u\n", size);
 }
 
 
